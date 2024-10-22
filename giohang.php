c018693a69dba695c6e13920434f9fcf2eac5769
Dichle2003 <ledich10031965@gmail.com>
2024-10-22 21:35:27 +0700

    init project

--- giohang.php
+++ giohang.php
@@ -27,35 +27,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['namepr']) && isset($_P
     $_SESSION['giohang'] = [];
     echo "Đơn hàng của bạn đã được ghi nhận.";
 }
-
-// Xóa sản phẩm khỏi giỏ hàng
+?>
+<?php
+if (!isset($_SESSION["giohang"]))  $_SESSION["giohang"]=[];
+if(isset($_POST['addcart'])&&($_POST['addcart'])){
+    $hinh=$_POST['hinh'];
+    $ten=$_POST['tensp'];
+    $gia=$_POST['gia'];
+    $soluong=$_POST['soluong'];
+    $sp =[$hinh,$ten,$gia,$soluong];
+    $_SESSION['giohang'][]=$sp;
+    
+   
+}
 if (isset($_POST['delcart']) && ($_POST['delcart'])) {
     $id = $_POST['id'];
     if (isset($_SESSION['giohang'][$id])) {
         unset($_SESSION['giohang'][$id]);
-        $_SESSION['giohang'] = array_values($_SESSION['giohang']); // Cập nhật lại chỉ số mảng
+        $_SESSION['giohang'] = array_values($_SESSION['giohang']); 
     }
 }
-
 // Hàm hiển thị các sản phẩm trong giỏ hàng
 function showgiohang() {
+    
     if (isset($_SESSION['giohang']) && is_array($_SESSION['giohang']) && !empty($_SESSION['giohang'])) {
-        for ($i = 0; $i < sizeof($_SESSION['giohang']); $i++) {
-            echo '<div class="cart-item">';
-            echo $i + 1;
-            echo ' <img src="' . $_SESSION['giohang'][$i][0] . '" alt=""><br>';
-            echo 'Tên sản phẩm: ' . htmlspecialchars($_SESSION['giohang'][$i][1]) . '<br>';
-            echo 'Giá: $' . $_SESSION['giohang'][$i][2] . '<br>';
-            echo 'Số lượng: ' . $_SESSION['giohang'][$i][3] . '<br>';
-            echo 'Tổng: ' . $_SESSION['giohang'][$i][3] * $_SESSION['giohang'][$i][2] . '<br>';
+       for($i=0;$i<sizeof($_SESSION['giohang']);$i++) {
+        echo '<div>';
+        $tt=$_SESSION['giohang'][ $i ][2]*$_SESSION['giohang'][$i][3];
+        echo '<div class="cart-item">';
+        echo $i + 1;
+        echo ' <img src="' . $_SESSION['giohang'][$i][0] . '" alt=""><br>';
+        echo 'Tên sản phẩm: ' . htmlspecialchars($_SESSION['giohang'][$i][1]) . '<br>';
+        echo 'Giá: $' . $_SESSION['giohang'][$i][2] . '<br>';
+        echo 'Số lượng: ' . $_SESSION['giohang'][$i][3] . '<br>';
+        echo 'Tổng: ' . $_SESSION['giohang'][$i][3] * $_SESSION['giohang'][$i][2] . '<br>';
 
-            // Nút xóa sản phẩm
-            echo '<form action="" method="post">';
-            echo '<input type="hidden" name="id" value="' . $i . '">';
-            echo '<button type="submit" name="delcart" value="1">Xóa</button>';
-            echo '</form>';
+        
+        echo '<form action="" method="post">';
+        echo '<input type="hidden" name="id" value="' . $i . '">';
+        echo '<button type="submit" name="delcart" value="1">Xóa</button>';
+        echo '</form>';
 
-            echo '</div><br>';
+        echo '</div><br>';
         }
     } else {
         echo "Giỏ hàng của bạn đang trống.";
@@ -65,20 +78,20 @@ function showgiohang() {
 
 <!DOCTYPE html>
 <html lang="vi">
+    <link rel="stylesheet" href="giohang.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giỏ hàng của bạn</title>
-    <link rel="stylesheet" href="giohang.css">
 </head>
 <body>
     <h1>Giỏ hàng của bạn</h1>
     <?php showgiohang(); ?>
     <form action="" method="post">
-        <input type="text" name="namepr" placeholder="Tên Người Nhận">
-        <input type="text" name="phone" placeholder="Số điện thoại người nhận">
-        <input type="text" name="address" placeholder="Địa chỉ người nhận">
-        <button type="submit">Mua Hàng</button>
+        <input type="text" name="namepr" id="" placeholder="Tên Người Nhận">
+        <input type="text" name="phone" id="" placeholder="số điện thoại người nhận">
+        <input type="text" name="address" placeholder="địa chỉ người nhận">
+    <button type="submit"> Mua Hàng</button>
     </form>
     <a href="aowebdautay.html">Tiếp tục mua sắm</a>
 </body>
